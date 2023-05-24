f81e73a0cf4e24bdb2739158133a3ddd1956e72d
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2023-05-24 18:11:56 +0530

    n=int(input()) l=list(map(int,input().split())) n1=int(input()) l1=[] for i in l:     if i+n1>=max(l):         print("True",end=' ')     else:         print("False",end=' ')

---
+++ Greatest_Number_of_Candies.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+n1=int(input())
+l1=[]
+for i in l:
+    if i+n1>=max(l):
+        print("True",end=' ')
+    else:
+        print("False",end=' ')
+
+
+ 
